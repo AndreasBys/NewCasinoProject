0249a7fc2d4baf8b5f1b30aa4eb46546a70e7e97
Abdey Habdey <andreaszzz51@gmail.com>
2023-02-21 14:45:13 +0100

    Navbar

--- Casino Project/CasinoClient/src/app/navbar.component.ts
+++ Casino Project/CasinoClient/src/app/navbar.component.ts
@@ -10,20 +10,52 @@ import { Router, RouterModule } from '@angular/router';
     <nav class="navbar navbar-scroll">
       <div class="container-fluid">
         <a class="navbar-brand">ABCCINO</a>
+        <div class="nav-links">
+          <ul>
+            <li class="nav-item">
+              <a class="nav-link" href="">Home</a>
+            </li>
+          </ul>
+          <ul class="navlogin">
+            <a class="nav-link">test</a>
+          </ul>
+        </div>
       </div>
+      
 
     </nav>
   `,
   styles: [`
-
+  li{
+    list-style-type: none;
+  }
+  .navlogin{
+    margin-right:auto!important;
+  }
   .navbar{
     position:fixed;
     z-index:1;
     width:100%;
     height:50px;
   }
+  .nav-links{
+    display:flex;
+    flex-basis:auto;
+    flex-grow: 1;
+  }
   .container-fluid{
     height:100%;
+    width:100%;
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    padding-left:2%;
+    padding-right:2%;
+  }
+  .navbar-brand{
+  }
+  a{
+    text-decoration: none;
   }
 
   @media (max-width: 911px){
